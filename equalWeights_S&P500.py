3c42bbda61e13f2bdea0f616f2818dc97d765afc
Sagar <56674688+sagar-tan@users.noreply.github.com>
2025-11-23 19:35:01 +0530

    Fetching Prices

--- equalWeights_S&P500.py
+++ equalWeights_S&P500.py
@@ -13,3 +13,14 @@ sp500_ticker = list(sp500_ticker) # converting the ticker data into a list
 print(f"Loaded {len(sp500_ticker)} tickers from S&P 500")
 
 #fetch Prices
+
+print("Downloading latest prices, this may take a few seconds...")
+prices = yf.download(sp500_ticker, period="1d")["Close"].iloc[0]
+
+df = pd.DataFrame({
+    "Ticker" : prices.index,
+    "Price": prices.values
+})
+
+# EQUAL Weights
+
