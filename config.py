b92a21f16607ea79ec558cd14e2114dded0fdf98
Sagar <56674688+sagar-tan@users.noreply.github.com>
2025-11-24 11:27:29 +0530

    Config+structure update

---
+++ config.py
@@ -0,0 +1,28 @@
+PORTFOLIO_SIZE = 100000
+ALLOW_FRACTIONAL = False
+OUTPUT_FILE = "equal_weight_filtered.xlsx"
+#adding the filter settings
+FILTERS = {
+    "price": {"min_price":5, "max_price": None},
+    "volatility": {"window_days":60, "max_vol": 0.05},
+    "pe": {"max_pe":50},
+    "dividend": {"min_yeild":0.0},
+    "sector": {"include":None, "exclude":[]},
+    "momentum": {"months":3, "min_return": 0.0},
+    "volume": {"min_avg_volume":100000},
+    "marketcap": {"min_mcap":1e9},
+    "beta": {"max_beta": 2.0},
+}
+
+#pipeline order: order in which the filters will be applied
+
+PIPELINE = [
+    "price",
+    "volume",
+    "market_cap",
+    "pe",
+    "dividend",
+    "beta",
+    "volatility",
+    "sector",
+]
